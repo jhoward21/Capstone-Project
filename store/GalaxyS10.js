d691ee8eb5fc273e45818beee0a6c6aedd4f1b3c
Latasha Howard <l_howard@outlook.com>
2020-03-25 20:56:09 -0500

    update4.2

--- store/GalaxyS10.js
+++ store/GalaxyS10.js
@@ -1,10 +1,33 @@
 export default {
   ID: "GalaxyS10",
   view: "Phone",
-  screenSize: "5inches",
+  screenSize: "6.4 inches",
   photo:
     "https://fdn.gsmarena.com/imgroot/reviews/19/samsung-galaxy-s10-plus/lifestyle/-728w2/gsmarena_025.jpg",
-  name: "Galaxy S10+",
+  name: "Samsung Galaxy S10+",
   info:
-    "Samsung's 2019 flagship is widely considered the best phone for everyone"
+    "Samsung's 2019 flagship is widely considered the best phone for everyone, due to it's versatility in software and hardware features.",
+  titleSpecs: "Specifications",
+  Specs: "6.4 inch 3040x1440 Dynamic Amoled HDR10+ display",
+  Specs1:
+    "CPU: Octa-core Snapdragon 855 64-bit(7nm) 2.84Ghz, GPU: Andreno 640 600Mhz",
+  Specs2: "Storage & Ram: 128GB/8GB Ram, 512GB/8GB Ram, 1TB/12GB Ram",
+  Specs3: "Headphone-jack, Dolby Stero Speakers, and comes with AKG headphones",
+  OS: "Android 10, One UI 2.1",
+  BC:
+    "4100 mAh Battery, 15W Fast Charging, Fast wireless charging, and reverse wireless charging.",
+  CSpec: "Camera:",
+  C1: "12 MP, f/1.5-2.4, 26mm (wide)",
+  C2: "12 MP, f/2.4, 52mm (telephoto)",
+  C3: "16 MP, f/2.2, 12mm (ultrawide)",
+  CSpec2: "Dual Selfie-Camera",
+  FC1: "10 MP, f/1.9, 26mm (wide)",
+  FC2: "8 MP, f/2.2, 22mm (wide)",
+  Video: "Video",
+  V: "4K 60/30fps HDR10+, 1440p 30fps, 1080p 60/30fps, and Super-Slowmo",
+  FVideo: "FrontCam-Video",
+  FV: "4K 60/30fps, 1080p 30fps",
+  YT: "YouTube Video:",
+  YouTube: "https://www.youtube.com/embed/Pmn7VRyB9Xg",
+  Purchase: "Links To Purchase The Device"
 };
