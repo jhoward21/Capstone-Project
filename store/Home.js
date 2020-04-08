d28c65970462e0b018991a059fb45a87ec0a8611
jhoward21 <afootball.life26@gmail.com>
2020-04-08 18:31:36 -0500

    update5.0.2

--- store/Home.js
+++ store/Home.js
@@ -7,7 +7,7 @@ export default {
     "https://www.androidcentral.com/sites/androidcentral.com/files/styles/mediumplus/public/article_images/2020/02/galaxy-s20-ultra-multitasking.jpg?itok=4HH664AE",
   info:
     "Samsung's late 2019  Note flagship is widely considered the best phone all around smartphone, or the phone that could do everything. Due to it's versatility in software and hardware features. In addition to it's unique S-Pen that makes note taking, writing phone numbers, and accessing the camera quickly. Another thing with the Note 10+ is you'll never have to worry about battery, as most screen on test show the device screen staying on, on a single charge form 7 1/2 to 8 hours! The Note 10+ also sports the same useful reverse wireless charging feature as the Galaxy S10+. When a friends battery is dead you can save the day, and juice their phone up by charing it with your Note10! To enjoy this feature rich device just be pre-pared to break the bank as the base model starts at $1099!",
-  name2: "Huawei P40 Pro+",
+  name2: "Huawei P30 Pro+",
   photo2:
     "https://www.androidcentral.com/sites/androidcentral.com/files/styles/mediumplus/public/article_images/2020/03/huawei-p40-pro-preview-3.jpg?itok=pBS8jad0",
   info2:
