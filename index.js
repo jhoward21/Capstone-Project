8383580e44265bcf0ac3c74c6965f2d4395e5d23
jhoward21 <afootball.life26@gmail.com>
2020-04-12 19:41:30 -0500

    update5.1.2

--- index.js
+++ index.js
@@ -29,14 +29,21 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
-  //Toggle Nav
 
   burger.addEventListener("click", () => {
+    //Toggle Nav
     nav.classList.toggle("nav-active");
-  });
-  //Animate Links
-  navLinks.forEach((link, index) => {
-    link.style.animation = `navLinks 0.5s ease forwards ${index / 7 + 0.3}s`;
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.6s ease forwards ${index / 7 +
+          0}s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle("toggle");
   });
 };
 
